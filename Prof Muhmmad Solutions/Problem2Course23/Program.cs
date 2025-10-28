df510ddbac84c7d3b328bb09132be38d05bdb3bb
Wael Kendouli <kendouliwael@gmail.com>
2025-10-28 17:11:27 +0100

    Add files via upload

---
+++ Prof Muhmmad Solutions/Problem2Course23/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static string DecimalToBinary(int number)
+    {
+        Stack<int> stack = new Stack<int>();
+
+        while (number > 0)
+        {
+            stack.Push(number % 2);
+            number /= 2;
+        }
+
+        return string.Join("", stack);
+    }
+
+    static void Main()
+    {
+        Console.WriteLine(DecimalToBinary(10)); // Output: 1010
+        Console.ReadKey();
+    }
+}
