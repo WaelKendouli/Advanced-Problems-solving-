344716ed45634c87057f83f01ad4b1b959bdeeae
Wael Kendouli <kendouliwael@gmail.com>
2025-11-01 13:28:46 +0100

    Add files via upload

---
+++ My solutions/Stacks and queues/Problem10Course23/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem10Course23
+{
+    internal class Program
+    {
+
+        class clsMyQueue
+        {
+            private Queue<int> _MyQueue = new Queue<int>();
+            private Stack<int> _stkMyStack = new Stack<int>();
+
+            public void Add(int number)
+            {
+                _MyQueue.Enqueue(number);
+            }
+
+            public void Show()
+            {
+                if (_MyQueue.Count==0)
+                {
+                    Console.WriteLine("Queue is empty");
+                    return;
+                }
+                Console.WriteLine(" Content of my queue : [" + string.Join(",", _MyQueue) + "]");
+            }
+
+            public void Reverse()
+            {
+                if (_MyQueue.Count==0)
+                {
+                    return;
+                }
+                while (_MyQueue.Count>0)
+                {
+                    _stkMyStack.Push(_MyQueue.Dequeue());
+                }
+                while (_stkMyStack.Count>0)
+                {
+                    _MyQueue.Enqueue(_stkMyStack.Pop());
+                }
+
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+            clsMyQueue MyQueue = new clsMyQueue();
+            MyQueue.Add(1);
+            MyQueue.Add(2);
+            MyQueue.Add(3);
+            MyQueue.Add(4);
+            MyQueue.Add(5);
+            Console.WriteLine("Before reversing : ");
+            MyQueue.Show();
+            MyQueue.Reverse();
+            Console.WriteLine("After reversing : ");
+            MyQueue.Show();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ My solutions/Stacks and queues/Problem9Course23/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem9Course23
+{
+    internal class Program
+    {
+
+        class clsCustomersScheduling
+        {
+            private Queue<string> _CustomersQueue = new Queue<string>();
+
+            public void AddCustomer(string Customer)
+            {
+                _CustomersQueue.Enqueue(Customer);
+            }
+
+            public void Serve()
+            {
+                if (_CustomersQueue.Count == 0)
+                {
+                    Console.WriteLine("No Customers to serve");
+                    return;
+                }
+                Console.WriteLine("Serving customers ... \n");
+
+                while (_CustomersQueue.Count > 0)
+                {
+                    Console.Write($"Serving {_CustomersQueue.Dequeue()}\n\n");
+
+                }
+            }
+
+
+        }
+        static void Main(string[] args)
+        {
+            clsCustomersScheduling Service = new clsCustomersScheduling();
+
+            Service.AddCustomer("Wael");
+            Service.AddCustomer("Hamid");
+            Service.AddCustomer("Sami");
+            Service.AddCustomer("James");
+
+            Service.Serve();
+
+            Console.ReadLine();
+
+        }
+    }
+}
